93160b9d2574f3de0c3ba2d22dab1a84baffe3ce
DinaraDervel <dinaradervel@gmail.com>
2024-02-19 12:38:45 -0600

    add dispatch to store

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,12 +9,12 @@ const MyPosts = (props) => {
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.updateNewPostText(text);
+    props.dispatch({ type: "UPDATE-NEW-POST-TEXT", newText: text });
   };
 
   let newPostElement = React.createRef();
   let addPost = () => {
-    props.addPost();
+    props.dispatch({ type: "ADD-POST" });
   };
 
   return (
