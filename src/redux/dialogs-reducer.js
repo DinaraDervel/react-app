839136c661fd3bca9853bfb1399b4ff18eb6f31b
DinaraDervel <dinaradervel@gmail.com>
2024-03-13 18:26:21 -0600

    refactored reducers

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -19,19 +19,20 @@ let initialState = {
 
 export const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY: {
-            let stateCopy = { ...state };
-            stateCopy.newMessageBody = action.body;
-            return stateCopy;
-        }
-        case SEND_MESSAGE: {
-            let stateCopy = { ...state };
-            stateCopy.messages = [...state.messages];
-            let body = stateCopy.newMessageBody;
-            stateCopy.newMessageBody = '';
-            stateCopy.messages.push({ id: 4, message: body });
-            return stateCopy;
-        }
+        case UPDATE_NEW_MESSAGE_BODY:
+            return {
+                ...state,
+                newMessageBody: action.body
+            };
+
+        case SEND_MESSAGE:
+            let body = state.newMessageBody;
+            return {
+                ...state,
+                newMessageBody: "",
+                messages: [...state.messages, { id: 4, message: body }]
+            };
+
         default: return state;
     }
 }
