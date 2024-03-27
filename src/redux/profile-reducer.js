19f224e4a6bc5931a06407c01e4d170a6cf8df73
DinaraDervel <dinaradervel@gmail.com>
2024-03-27 17:02:25 -0600

    add profile

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     posts: [
@@ -11,6 +12,7 @@ let initialState = {
         { id: 2, message: "Hi! It's my first post", likesCount: 11 },
     ],
     newPostText: 'it-kamasutra.com',
+    profile: null,
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -34,11 +36,19 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText
             };
         }
+
+        case SET_USER_PROFILE: {
+            return {
+                ...state,
+                profile: action.profile
+            };
+        }
         default: return state;
     }
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 
 export default profileReducer;
