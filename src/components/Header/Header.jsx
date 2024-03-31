9944b26d6fdb97ee573f49ab2035c60d5348d3a1
DinaraDervel <dinaradervel@gmail.com>
2024-03-31 13:36:07 -0600

    add login block

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import s from './Header.module.css';
+import React from "react";
+import s from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
-    return <header className={s.header}>
-        <div className={s.logo}><img src="https://marketplace.canva.com/BEZO4/MADhH_BEZO4/3/tl/canva-asteroid-icon-MADhH_BEZO4.png" alt='logo' /></div>
-      </header>;
-}
+const Header = (props) => {
+  return (
+    <header className={s.header}>
+      <div className={s.logo}>
+        <img
+          src="https://marketplace.canva.com/BEZO4/MADhH_BEZO4/3/tl/canva-asteroid-icon-MADhH_BEZO4.png"
+          alt="logo"
+        />
+      </div>
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
